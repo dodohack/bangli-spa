64928b366445e89467fcca48f243c8d4b116cdec
hualin.wu <hualin.wu@imgtec.com>
2017-09-18 16:54:39 +0100

    Add offer-card widget, wait on styling it

--- app/offer/offer.module.ts
+++ app/offer/offer.module.ts
@@ -5,8 +5,10 @@ import { OfferRoutes }    from './routes';
 
 import { OfferTopic }   from './offer.topic';
 import { OfferIndex }   from './offer.index';
+import { OfferCard }    from "./widgets/card";
 
 export const COMPONENTS = [
+    OfferCard,
     OfferTopic,
     OfferIndex,
 ];
---
+++ app/offer/widgets/card.ts
@@ -0,0 +1,16 @@
+/**
+ * The card widget used to display one offer
+ */
+
+import { Component }               from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+
+@Component ({
+    selector: 'offer-card',
+    templateUrl: './card.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OfferCard
+{
+
+}
