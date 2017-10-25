46c432986ef737741f31a08db2d23ab30c1f93a5
Aries Wu <aries@bangli.uk>
2017-10-25 23:12:30 +0100

    Add @fadeIn animation to offer-card

--- src/app/common/offer-card.ts
+++ src/app/common/offer-card.ts
@@ -8,10 +8,29 @@ import { ChangeDetectionStrategy } from '@angular/core';
 import { Entity }                  from "../core/models";
 import { Helper }                  from "../core/helper";
 
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition
+} from '@angular/animations';
+
+
+
 @Component ({
     selector: 'offer-card',
     templateUrl: './offer-card.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [
+        trigger('fadeIn', [
+            state('in', style({ opacity: '1'})),
+            transition(':enter', [
+                style({ opacity: '0'}),
+                animate(500),
+            ]),
+        ]),
+    ],
 })
 export class OfferCard
 {
