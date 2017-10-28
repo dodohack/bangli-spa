83fe5b1deed9d34b6b682d9ff53fca52413480f0
Aries Wu <aries@bangli.uk>
2017-10-28 12:10:54 +0100

    Add build-release.sh to build production release

---
+++ build-release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Build release package and copy
+
+npm run build-release
+
+# Replace files in folder when build success
+if [ $? -eq 0 ]; then
+
+    # Make 'dist' if it does not exist
+    if [ ! -d dist ]; then 
+	mkdir dist
+    fi
+
+    rm -rf dist/*
+    cp -r build/* dist/
+fi
